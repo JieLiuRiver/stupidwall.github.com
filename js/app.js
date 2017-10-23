d55db5bec9f659a68443b795d92a9a1f08c347f8
heliujie <268079@163.com>
2017-10-23 17:36:15 +0800

    update files

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        worklist: [
+            {
+                url: 'https//baidu.com',
+                name: '菜鸟网络'
+            }
+        ]
+    }
+})
