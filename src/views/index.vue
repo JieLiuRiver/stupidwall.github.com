1542b2f96f183918524d4884a3d3e3c372196840
stupidWall <268079@163.com>
2017-10-24 16:21:25 +0800

    change cli

---
+++ src/views/index.vue
@@ -0,0 +1,56 @@
+<style scoped>
+    .index {
+        width: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        text-align: center;
+    }
+
+    .index h1 {
+        height: 150px;
+    }
+
+    .index h1 img {
+        height: 100%;
+    }
+
+    .index h2 {
+        color: #666;
+        margin-bottom: 200px;
+    }
+
+    .index h2 p {
+        margin: 0 0 50px;
+    }
+
+    .index .ivu-row-flex {
+        height: 100%;
+    }
+</style>
+<template>
+    <div class="index">
+        <Menu mode="horizontal" :theme="theme" active-name="1">
+            <MenuItem name="1">
+                <Icon type="ios-paper"></Icon>
+                内容管理
+            </MenuItem>
+            <MenuItem name="2">
+                <Icon type="ios-people"></Icon>
+                用户管理
+            </MenuItem>
+            <MenuItem name="3">
+                <Icon type="settings"></Icon>
+                综合设置
+            </MenuItem>
+        </Menu>
+    </div>
+</template>
+<script>
+    export default {
+        methods: {
+            theme: 'primary'
+        }
+    };
+</script>
